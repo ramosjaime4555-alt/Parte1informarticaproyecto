4c9d7e3c48bc9c8d6313512a914954afc910d47f
Jonathan_Segura <ramosjaime4555@gmail.com>
2026-08-31 13:01:26 -0600

    creacion de iniciio de sesion

--- codigo.js
+++ codigo.js
@@ -1,3 +1,6 @@
+let contra = ""
+let correoinicio = ""
+
 function acceder(){
     let ingreso_correo = document.getElementById("ingreso_correo").value;
     let ingreso_contraseña = document.getElementById("ingreso_contraseña").value;
@@ -5,10 +8,29 @@ function acceder(){
 
     if (ingreso_contraseña == confirmar_contraseña && ingreso_correo != "" ){
         alert("Sea registrado con exito")
+        correoinicio = ingreso_correo
+        contra = confirmar_contraseña
     }
     if (ingreso_contraseña != confirmar_contraseña){
         alert("La contraseña no coincide")
     }if(ingreso_contraseña == "" || ingreso_correo == "" || confirmar_contraseña == ""){
         alert("Uno o mas campos esta vacio")
+    }
+}
+
+function leerdatos(){
+    console.log(contra)
+    console.log(correoinicio)
+}
+
+function inicio(){
+    let correo = document.getElementById("correo")
+    let contraseña  = document.getElementById("contraseña")
+
+    if (contraseña == contra){
+        alert("JKJKJKJ")
+    }
+    if (correo == correoinicio){
+        alert("fjdkfjdkfdj")
     }
 }
