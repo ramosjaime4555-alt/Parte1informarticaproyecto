5c9e4632ebedf1f8e3efd86a5300ce6b20f3c745
Jonathan_Segura <ramosjaime4555@gmail.com>
2026-09-02 07:14:59 -0600

    limpiar campos

--- codigo.js
+++ codigo.js
@@ -17,6 +17,7 @@ function acceder(){
         alert("Se ha registrado con éxito");
         contra = confirmar_contraseña;   
         correoinicio = ingreso_correo;    
+        acceder.clear()
     } else {
         alert("No coincide la contraseña");
     }
