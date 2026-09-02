080dddc78c878dfba5da25179a920671974ba08a
Jonathan_Segura <ramosjaime4555@gmail.com>
2026-09-02 07:19:16 -0600

    arrglo de error

--- codigo.js
+++ codigo.js
@@ -16,8 +16,7 @@ function acceder(){
     if (ingreso_contraseña == confirmar_contraseña){
         alert("Se ha registrado con éxito");
         contra = confirmar_contraseña;   
-        correoinicio = ingreso_correo;    
-        acceder.clear(acceder)
+        correoinicio = ingreso_correo;   
     } else {
         alert("No coincide la contraseña");
     }
